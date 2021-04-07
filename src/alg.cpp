6c9b4faca3ad1b044416997f1881b12a34fd3b66
smirnovaekaterina <75855500+smirnovaekaterina@users.noreply.github.com>
2021-04-07 23:36:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,9 @@
 // Copyright 2021 NNTU-CS
-#include "alg.h"
-
 int countPairs1(int *arr, int len, int value) {
   int pairs = 0;
   for (int i = 0; i < len - 1; i++) {
     for (int j = i + 1; j < len; j++) {
-      if (arr[i] + arr[j] == value)
+      if (arr[i] + arr[j] == value) {
         pairs++;
       }
     }
